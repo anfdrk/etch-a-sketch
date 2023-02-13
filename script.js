ca1bc6647e9667b801af17cfbc09f2134b8d446b
Andrei Fedaruk <anfdrk@gmail.com>
2023-02-13 03:46:18 +0300

    Create shade function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const gridContainer = document.querySelector('.grid-container')
 const slider = document.getElementById('slider')
 const colorBtn = document.getElementById('color')
 const rainbowBtn = document.getElementById('rainbow')
-const greyScaleBtn = document.getElementById('grey-scale')
+const shadeBtn = document.getElementById('shade')
 const eraserBtn = document.getElementById('eraser')
 const clearBtn = document.getElementById('clear')
 const colorInput = document.getElementById("color-input");
@@ -10,11 +10,12 @@ const colorInput = document.getElementById("color-input");
 let currentColor = colorInput.value
 let currentMode = 'color'
 colorBtn.classList.add('active')
+slider.value = 38
 createGrid(slider.value)
 
 colorBtn.onclick = () => setCurrentMode('color')
 rainbowBtn.onclick = () => setCurrentMode('rainbow')
-greyScaleBtn.onclick = () => setCurrentMode('grey scale')
+shadeBtn.onclick = () => setCurrentMode('shade')
 eraserBtn.onclick = () => setCurrentMode('eraser')
 clearBtn.onclick = () => clearGrid()
 
@@ -30,8 +31,8 @@ function activateButton(newMode) {
     colorBtn.classList.remove('active')
   } else if (currentMode === 'eraser') {
     eraserBtn.classList.remove('active')
-  } else if (currentMode === 'grey scale') {
-    greyScaleBtn.classList.remove('active')
+  } else if (currentMode === 'shade') {
+    shadeBtn.classList.remove('active')
   }
 
   if (newMode === 'rainbow') {
@@ -40,8 +41,8 @@ function activateButton(newMode) {
     colorBtn.classList.add('active')
   } else if (newMode === 'eraser') {
     eraserBtn.classList.add('active')
-  } else if (newMode === 'grey scale') {
-    greyScaleBtn.classList.add('active')
+  } else if (newMode === 'shade') {
+    shadeBtn.classList.add('active')
   }
 }
 
@@ -56,12 +57,12 @@ function createGrid(size) {
       const cell = document.createElement("div")
       cell.classList.add("grid-cell")
       cell.addEventListener("mousedown", () => {
-        chooseColor()
+        chooseColor(cell.style.backgroundColor)
         cell.style.backgroundColor = currentColor
       })
       cell.addEventListener("mouseover", (event) => {
         if (event.buttons === 1) {
-          chooseColor()
+          chooseColor(cell.style.backgroundColor)
           cell.style.backgroundColor = currentColor
         }
       })
@@ -73,18 +74,16 @@ function createGrid(size) {
   }
 }
 
-function chooseColor(info) {
-  print(info)
+function chooseColor(backgroundColor) {
   if (currentMode === 'rainbow') {
     currentColor = getRandomColor()
-  } else if (currentMode === 'grey scale'){
-    currentColor = getShade()
+  } else if (currentMode === 'shade'){
+    currentColor = getShade(backgroundColor)
   } else if (currentMode === 'eraser') {
     currentColor = '#CFD6D0'
   } else if (currentMode === 'color') {
     currentColor = colorInput.value
   }
-
 }
 
 function clearGrid() {
@@ -96,8 +95,13 @@ function getRandomColor() {
   return `hsl(${Math.random() * 360}, 100%, 70%)`;
 }
 
-// --------------------------------------------------------------
-
-function print(x){
-  console.log(x)
+function getShade(backgroundColor) {
+  if (backgroundColor === '') {
+    backgroundColor = 'rgb(223, 224, 223)'
+  }
+  const [r, g, b] = backgroundColor.match(/\d+/g).map(Number);
+  const darkerR = Math.max(0, r - (r * 0.17));
+  const darkerG = Math.max(0, g - (g * 0.17));
+  const darkerB = Math.max(0, b - (b * 0.17));
+  return `rgb(${darkerR}, ${darkerG}, ${darkerB})`;
 }
